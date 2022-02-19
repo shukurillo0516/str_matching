27bd658d326ea68a377f11e08e90f2db306c7c42
shukurullo <shukurullo0516@gmail.com>
2022-02-19 18:32:04 -0500

    Updating

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 			Done 3. Determine the alphabet
 			Done 4. Path the user's input to matching func with appropriate data
 """
+import re
 import string
 
 from fuzzywuzzy import fuzz
@@ -41,7 +42,7 @@ def get_region():
 	return region
 
 
-def det_alphabet(tuman, data):
+def det_alphabet(tuman, data, step):
 	"""
 	This func takes word, and tumans
 	Checks the alphabet if the alphabet is cyrilic, returns list of cyrilic names
@@ -55,10 +56,47 @@ def det_alphabet(tuman, data):
 			ind = 0
 			break
 
-	for i in range(ind, len(data), 3):
-		res.append(data[i])		
+	for i in range(ind, len(data), step):
+		res.append(data[i].lower())		
 			
-	return res
+	return res, ind
+
+
+def rectify(u_tuman):
+	u_tuman = u_tuman.lower() + " "
+	u_tuman = u_tuman.replace(",", " ").replace(" t ", " tumani ").replace(" sh", " shahar ")
+	u_tuman = u_tuman.replace(" т ", " тумани ").replace(" ш ", " шаҳри ")
+	# Replace viloyat
+	u_tuman = u_tuman.replace("viloyati", "").replace("viloyat", "").replace(" v ", "")
+
+	return u_tuman
+
+
+def extract_tuman(region, u_tuman, ind):
+	"""
+	This function splits u_tuman, and finds the most matching region adn deletes the region.
+	"""
+	region_tr, ind = det_alphabet(u_tuman, REGIONS[int(region)-1][1:], 3)
+
+	# dictionary (key index of an item, value is the matching percent)
+	values = dict()
+	
+	#split u_tuman
+	arr_tuman = u_tuman.split()
+	#loop through item and add index and matching percent to the dict
+	temp = int()
+	for i, item in enumerate(arr_tuman):
+		percent = fuzz.ratio(region_tr[0], item)
+		#push only the largest value
+		if temp < percent:
+			values = {i: percent}
+			temp = percent
+
+	#Poping the region
+	arr_tuman.pop(list(values)[0])
+
+	#return string
+	return " ".join(arr_tuman)
 
 
 def main():
@@ -75,10 +113,19 @@ def main():
 	data_tumans = DATA[region]
 	# Getting user tuman
 	u_tuman = input("Enter name of tuman: ").lower()
+	#delete regex patter and handle t and sh
+	u_tuman = rectify(u_tuman)
 	# Determine alphabet
-	data_tumans_tr = det_alphabet(u_tuman, data_tumans)
-	matched_tumanlar = process.extract(u_tuman, data_tumans_tr, limit=5)
+	data_tumans_tr, ind = det_alphabet(u_tuman, data_tumans, 3)
+	# extracting tuman name from string
+	e_name =  extract_tuman(region, u_tuman, ind)
+	
+	matched_tumanlar = process.extract(e_name, data_tumans_tr, limit=5)
+	print("########################## RESULT #################################")
 	print(matched_tumanlar)
+	print("###########################################################")
+	print()
 
 
-main()
+while True:
+	main()
